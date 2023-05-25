308aea5c239745ba8d1cd712a76103c80e4dd672
donleong94 <donleong94@gmail.com>
2023-05-25 17:30:44 +0800

    - Add envied encryption handling

---
+++ lib/env/env.dart
@@ -0,0 +1,12 @@
+import 'package:envied/envied.dart';
+
+part 'env.g.dart';
+
+@Envied(path: ".env")
+abstract class Env {
+  @EnviedField(varName: "API_KEY", obfuscate: true)
+  static final String apiKey = _Env.apiKey;
+
+  @EnviedField(varName: "API_SECRET", obfuscate: true)
+  static final String apiSecret = _Env.apiSecret;
+}
