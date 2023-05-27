b7ce6d81494eb574cd8bb026a9e2377195d3b9b1
donleong94 <donleong94@gmail.com>
2023-05-27 20:18:42 +0800

    - Update settings for Android - Add location service in home page

--- lib/feature/home/view/home_view.dart
+++ lib/feature/home/view/home_view.dart
@@ -1,6 +1,10 @@
+import 'dart:async';
+import 'dart:io';
+
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:localization/localization.dart';
 import 'package:sample_article_flutter/core/constant/value_constants.dart';
 import 'package:sample_article_flutter/core/style/app_color.dart';
@@ -15,9 +19,12 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
+  String _locationText = "";
+
   @override
   void initState() {
     checkLocationService();
+    _checkIsLocationEnable();
     super.initState();
   }
 
@@ -66,6 +73,11 @@ class _HomeViewState extends State<HomeView> {
             );
           },
         ),
+        24.ph,
+        Container(
+          padding: EdgeInsets.symmetric(horizontal: 16.h, vertical: 8.h),
+          child: _locationText.mediumText(),
+        ),
       ],
     );
   }
@@ -104,6 +116,69 @@ class _HomeViewState extends State<HomeView> {
           ),
         ],
       ),
+    );
+  }
+
+  void _checkIsLocationEnable() async {
+    bool isLocationServiceEnabled = await Geolocator.isLocationServiceEnabled();
+
+    if (isLocationServiceEnabled) {
+      _startLocationStream();
+    }
+
+    StreamSubscription<ServiceStatus> serviceStatusStream = Geolocator.getServiceStatusStream().listen(
+      (ServiceStatus status) {
+        if (status == ServiceStatus.enabled) {
+          _startLocationStream();
+          GeneralUtils.showGeneralToast("locationEnabledMsg".i18n());
+        } else {
+          GeneralUtils.showGeneralToast("locationDisabledMsg".i18n());
+        }
+      },
+    );
+  }
+
+  void _startLocationStream() async {
+    late LocationSettings locationSettings;
+
+    if (Platform.isAndroid) {
+      locationSettings = AndroidSettings(
+        accuracy: LocationAccuracy.high,
+        distanceFilter: 100,
+        forceLocationManager: true,
+        intervalDuration: const Duration(seconds: 10),
+        //(Optional) Set foreground notification config to keep the app alive
+        //when going to the background
+        foregroundNotificationConfig: const ForegroundNotificationConfig(
+          notificationText: "Example app will continue to receive your location even when you aren't using it",
+          notificationTitle: "Running in Background",
+          enableWakeLock: true,
+        ),
+      );
+    } else if (Platform.isIOS) {
+      locationSettings = AppleSettings(
+        accuracy: LocationAccuracy.high,
+        activityType: ActivityType.fitness,
+        distanceFilter: 100,
+        pauseLocationUpdatesAutomatically: true,
+        // Only set to true if our app will be started up in the background.
+        showBackgroundLocationIndicator: false,
+      );
+    } else {
+      locationSettings = const LocationSettings(
+        accuracy: LocationAccuracy.high,
+        distanceFilter: 100,
+      );
+    }
+
+    StreamSubscription<Position> positionStream = Geolocator.getPositionStream(locationSettings: locationSettings).listen(
+      (Position? position) {
+        if (position != null) {
+          setState(() {
+            _locationText = "Current position: ${position.latitude.toString()}, ${position.longitude.toString()}";
+          });
+        }
+      },
     );
   }
 }
